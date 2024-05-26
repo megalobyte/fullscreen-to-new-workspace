4a985d1525ee60243e92b71940b0722ce878676c
megalobyte <megalobyte@protonmail.com>
2024-05-26 12:53:51 -0400

    Update deploy script so it also enables the extension

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,10 @@
 
 NAME=fullscreen-to-empty-workspace@aiono.dev
 DIR=src
+
+gnome-extensions disable $NAME
+
 rm -rf ~/.local/share/gnome-shell/extensions/$NAME
 cp -r src ~/.local/share/gnome-shell/extensions/$NAME
+
+gnome-extensions enable $NAME
